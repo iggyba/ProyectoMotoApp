6eb2e22737281d1699c9959d4b0f2e76e18f1bac
samcastellon <49949334+samcastellon@users.noreply.github.com>
2019-05-31 10:16:04 -0400

    app con tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
   { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
+  { path: 'tabs', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
+  { path: 'tab2', loadChildren: './componentes/tab2/tab2.module#Tab2PageModule'},
+
 ];
 
 @NgModule({
