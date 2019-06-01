783e25629cf090a8797691ac47db597172aa892c
rafaartero3098 <rafaalvarez3098@gmail.com>
2019-06-01 12:57:32 -0400

    Intento de restauración

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,38 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
-  { path: 'datos', loadChildren: './componentes/datos/datos.module#DatosPageModule' },
   { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
-  { path: 'tabs', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
-  { path: 'registros', loadChildren: './componentes/registros/registros.module#RegistrosPageModule' },
 
-=======
-  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
->>>>>>> parent of 6eb2e22... app con tabs
-=======
-  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
->>>>>>> parent of 6eb2e22... app con tabs
-=======
-  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
->>>>>>> parent of 6eb2e22... app con tabs
-=======
-  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
->>>>>>> parent of 6eb2e22... app con tabs
 ];
 
 @NgModule({
