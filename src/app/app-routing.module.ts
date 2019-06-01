c7da4ff3b1a8319fa0b64735803a7406477afc43
rafaartero3098 <rafaalvarez3098@gmail.com>
2019-06-01 12:44:04 -0400

    Revirtiendo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+<<<<<<< HEAD
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
@@ -10,6 +11,11 @@ const routes: Routes = [
   { path: 'tabs', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
   { path: 'registros', loadChildren: './componentes/registros/registros.module#RegistrosPageModule' },
 
+=======
+  { path: '', redirectTo: 'confirmacion', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
+>>>>>>> parent of 6eb2e22... app con tabs
 ];
 
 @NgModule({
