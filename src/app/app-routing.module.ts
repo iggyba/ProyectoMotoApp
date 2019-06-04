8e1e0d5e197d7fda36f1c0f4b37ffb0beefcb3e2
Ignacio <iggy.b.q@gmail.com>
2019-06-04 09:51:15 -0400

    complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,20 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
-=======
   { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
->>>>>>> parent of 64ca764... Merge remote-tracking branch 'origin/branch2fabri' into sammy
-=======
-  { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
->>>>>>> parent of 64ca764... Merge remote-tracking branch 'origin/branch2fabri' into sammy
-=======
-  { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
->>>>>>> parent of 64ca764... Merge remote-tracking branch 'origin/branch2fabri' into sammy
   { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
 
 ];
