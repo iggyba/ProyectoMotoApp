eef0300017a23f7c4347413a20e57da50c1b69c4
Ignacio <iggy.b.q@gmail.com>
2019-06-04 09:47:51 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 <<<<<<< HEAD
+<<<<<<< HEAD
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
+=======
+  { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
+>>>>>>> parent of 64ca764... Merge remote-tracking branch 'origin/branch2fabri' into sammy
 =======
   { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
 >>>>>>> parent of 64ca764... Merge remote-tracking branch 'origin/branch2fabri' into sammy
