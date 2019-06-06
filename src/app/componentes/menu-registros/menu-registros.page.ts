1ac3a1026040a530aa75a66e1f0e679216b1d6dd
rafaartero3098 <rafaalvarez3098@gmail.com>
2019-06-05 23:13:17 -0400

    Implementación de menú para registros

--- src/app/componentes/menu-registros/menu-registros.page.ts
+++ src/app/componentes/menu-registros/menu-registros.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-menu-registros',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuRegistrosPage implements OnInit {
 
-  constructor() { }
+  constructor( public router : Router) { }
 
   ngOnInit() {
   }
 
+
+  registrarNuevoMotoTaxi(){
+    this.router.navigate(['/registros']);
+  }
+
+  listarMotoTaxis(){
+    this.router.navigate(['/registros']);
+  }
 }
