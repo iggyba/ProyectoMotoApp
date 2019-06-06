cba3a1a7766a44693bd4d3a987ec4c358112ad31
rafaartero3098 <rafaalvarez3098@gmail.com>
2019-06-05 23:00:46 -0400

    Agregando menu para registros

---
+++ src/app/componentes/menu-registros/menu-registros.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-registros',
+  templateUrl: './menu-registros.page.html',
+  styleUrls: ['./menu-registros.page.scss'],
+})
+export class MenuRegistrosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
