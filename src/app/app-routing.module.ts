5a35ef29de96cf98de192c16ac9259e248b02d30
Ignacio <iggy.b.q@gmail.com>
2019-06-11 08:51:03 -0400

    Changes Working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './componentes/tabs/tabs.module#TabsPageModule' },
+
+  { path: '', redirectTo: 'confir', pathMatch: 'full' },
   { path: 'confir', loadChildren: './componentes/confir/confir.module#ConfirPageModule' },
 
 ];
