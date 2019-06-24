cb8d1e3d58fdbe270f76bf8a27d026896aeda9b6
rafa3098 <rafaalvarez3098@gmail.com>
2019-06-24 17:38:24 -0400

    Arreglando errores de merge

--- src/app/pages/dashboard/datos/datos.page.ts
+++ src/app/pages/dashboard/datos/datos.page.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MototaxisService, motoTaxi } from "../../../servicios/mototaxis.service";
 import { Router } from "@angular/router";
-
 import { AuthService } from "../../../servicios/auth.service";
+import { AlertController } from '@ionic/angular';
+import { isNullOrUndefined } from 'util';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { map } from "rxjs/operators";
 
 @Component({
   selector: 'app-datos',
@@ -12,24 +15,43 @@ import { AuthService } from "../../../servicios/auth.service";
 export class DatosPage implements OnInit {
 
   public motoTaxisArreglo: motoTaxi;
+  public idUsuario: string;
 
-  constructor(public mototaxisService : MototaxisService, public router : Router, public auth: AuthService) { }
+  constructor(public mototaxisService: MototaxisService,
+    public router: Router,
+    public auth: AuthService,
+    public alertCtrl: AlertController) { }
 
   ngOnInit() {
-      this.mototaxisService.getMototaxiUsuario(this.auth.getUidUser()).subscribe(motoTaxis =>{
-      this.motoTaxisArreglo=motoTaxis;
-      })
-      //this.mototaxisService.getDatosMotoTaxis().subscribe( motoTaxis =>{
-      //this.motoTaxisArreglo=motoTaxis;
-    //})
+      this.mototaxisService.cambiarDisponibilidadTrue(this.auth.getUidUser());
+      this.mototaxisService.getMototaxiUsuario(this.auth.getUidUser()).subscribe(motoTaxis => {
+      this.motoTaxisArreglo = motoTaxis;
+    })
   }
+  
+  async desconectar() {
+    const alert = await this.alertCtrl.create({
+      header: 'Salir de sesión',
+      message: `¿Estás seguro de que deseas salir de sesión ${this.motoTaxisArreglo.nombreMotoTaxi}`,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancelar',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirmación Cancelada');
+          }
+        }, {
+          text: 'Confirmar',
+          handler: () => {
+            console.log('Confirmar');
+            this.auth.logout();
+            this.mototaxisService.cambiarDisponibilidadFalse(this.auth.getUidUser());
+          }
+        }
+      ]
+    });
+    await alert.present();
 
-  conectar(){
-    this.router.navigate(['/ubicacion']);
-  }
-
-  desconectar(){
-    this.mototaxisService.cambiarDisponibilidadFalse(this.motoTaxisArreglo.idMotoTaxi);
-    this.router.navigate(['/login']);
   }
 }
